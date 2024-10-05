8de75dd2be971d26061f9908ee9c03ff6cb09081
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2024-10-05 12:55:13 +0700

    Reserve gem name

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2024-10-05
+
+- Initial release
---
+++ lib/solid_queue_dashboard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "solid_queue_dashboard/version"
+
+module SolidQueueDashboard
+  class Error < StandardError; end
+  # Your code goes here...
+end
