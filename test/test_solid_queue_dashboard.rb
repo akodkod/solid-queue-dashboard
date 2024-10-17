705868f1c6615f63620a2b0227658c5450443348
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2024-10-17 15:41:29 +0700

    Fix a single test

--- test/test_solid_queue_dashboard.rb
+++ test/test_solid_queue_dashboard.rb
@@ -4,10 +4,6 @@ require "test_helper"
 
 class TestSolidQueueDashboard < Minitest::Test
   def test_that_it_has_a_version_number
-    assert_not_nil ::SolidQueueDashboard::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+    assert ::SolidQueueDashboard::VERSION
   end
 end
